690f0b6bea865749cea656c6f589d958b3ce8e42
lafamila <lafamila@naver.com>
2018-09-19 17:43:06 +0900

    보호자 관리

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 from config import query
 from flask import Flask, request, session, redirect, render_template, url_for
 from flask_socketio import SocketIO, send
+import json
 import os
+import copy
 app = Flask(__name__)
 socket = SocketIO(app)
 
-@app.route('/chat')
+@app.route('/chat', methods=['POST'])
 def chat():
-	return render_template('chat.html')
+    
+	return render_template('chat.html', hospt_id=session["hospital_id"])
 
 
 @socket.on("message")
 def message(msg):
+    req = json.loads(msg)
     print("message : " + msg)
     result = dict()
-    if msg == "new_connect":
+    if req["type"] == "connect":
+        print(req["id"])
         result['message'] = 'New User'
         result['type'] = 'connect'
     else:
@@ -49,6 +54,31 @@ def personal():
     q = "SELECT * FROM `pet` WHERE `PET_ID` = %s";
 
     return query(q, True, True, pet_id);
+
+@app.route('/allPerson', methods=['POST'])
+def allPerson():
+    hospt_id = request.form.get('hospt_id')
+    q = "SELECT * FROM `diagnosis` WHERE `HOSPITAL_ID` = %s ORDER BY `DIAGN_DATE` DESC"
+    diags = query(q, False, False, hospt_id)
+
+    contents = []
+
+    cache = {}
+    for diag in diags:
+        pet_id = diag['PET_ID']
+        if pet_id in cache:
+            pet = cache[pet_id]
+        else:
+            q = "SELECT * FROM `pet` WHERE `HOSPITAL_ID` = %s AND `PET_ID` = %s"
+            pet = query(q, True, False, hospt_id, pet_id)
+            cache[pet_id] = pet
+
+        content = copy.deepcopy(pet)
+        content['DIAGN_DATE'] = diag['DIAGN_DATE']
+        content['DIAGN_NAME'] = diag['DIAGN_NAME']
+        contents.append(content)
+    return json.dumps(contents)
+
 
 @app.route('/main', methods=['GET'])
 def main():
@@ -96,8 +126,20 @@ def dated_url_for(endpoint, **values):
             values['q'] = int(os.stat(file_path).st_mtime)
     return url_for(endpoint, **values)
 
+
+
+
+
+@app.route('/diag')
+def diag():
+    return render_template('diag.html')
+
+@app.route('/person')
+def person():
+    return render_template('person.html', hospt_id=session["hospital_id"])
+
 if __name__ == "__main__":
     app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
     #app.run(host="0.0.0.0", port=5000)
-    socket.run(app, port=5000, host='0.0.0.0')
-    # socket.run(app)
+    # socket.run(app, port=5000, host='0.0.0.0')
+    socket.run(app)
